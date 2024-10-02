dded8a81d4ec3c6773961939280fe02f0809a2ed
khatia <130936157+khatias@users.noreply.github.com>
2024-10-02 15:31:26 -0400

    Add blog page with image, title, description, and 'Continue Reading' link functionality

--- src/pages/Blog/BlogPage.jsx
+++ src/pages/Blog/BlogPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Header from '../../Header/Header';
+import Footer from '../../Footer/Footer';
+import BlogPageContent from './BlogContent';
+import './BlogContent.css'
 
 function BlogPage() {
   return (
-    <div>BlogPage</div>
+    <>
+    <Header />
+    <BlogPageContent />
+    <Footer />
+    </>
   )
 }
 
