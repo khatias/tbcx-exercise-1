78b0178d4c844995f6861695b045f504a2527799
DataShalam <d.shalamberidze04@gmail.com>
2024-12-26 16:19:01 +0400

    added CreateCheckoutSession which gets priceId and makes payment with stripe, in case of success it returns URl of product-result page

---
+++ src/app/actions/stripe-payment.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import type { Stripe } from "stripe";
+import { headers } from "next/headers";
+import { stripe } from "../../lib/stripe/stripe";
+
+export async function createCheckoutSession(
+  data: FormData
+): Promise<{ client_secret: string | null; url: string | null }> {
+  const ui_mode = data.get(
+    "uiMode"
+  ) as Stripe.Checkout.SessionCreateParams.UiMode;
+
+  const origin: string = (await headers()).get("origin") as string;
+
+  const locale = data.get("locale") || "en";
+
+  const priceId = data.get("priceId") as string;
+
+  if (!priceId) {
+    throw new Error("Price ID is required.");
+  }
+
+  const checkoutSession: Stripe.Checkout.Session =
+    await stripe.checkout.sessions.create({
+      mode: "payment",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: priceId,
+          quantity: 1,
+        },
+      ],
+      ...(ui_mode === "hosted" && {
+        success_url: `${origin}/${locale}/pricing/product-result?session_id={CHECKOUT_SESSION_ID}`,
+      }),
+      ...(ui_mode === "embedded" && {
+        return_url: `${origin}/${locale}/subscribe/embedded?session_id={CHECKOUT_SESSION_ID}`,
+      }),
+      ui_mode,
+    });
+
+  return {
+    client_secret: checkoutSession.client_secret,
+    url: checkoutSession.url,
+  };
+}
+
+export async function createPaymentIntent(
+  data: FormData
+): Promise<{ client_secret: string }> {
+  throw new Error("PaymentIntent is not used for subscriptions.");
+}
