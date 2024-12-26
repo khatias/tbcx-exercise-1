2906211cc8109f415a1634448013c02db5e5fabf
g.dalalishvili <dalalishviligiorgi36@gmail.com>
2024-12-26 23:10:31 +0400

    feat: add stripe payment feature and added orders

--- src/app/actions/stripe-payment.ts
+++ src/app/actions/stripe-payment.ts
@@ -1,26 +1,33 @@
 "use server";
-
+ 
 import type { Stripe } from "stripe";
 import { headers } from "next/headers";
 import { stripe } from "../../lib/stripe/stripe";
-
+import { createClient } from "@/src/utils/supabase/server";
+ 
 export async function createCheckoutSession(
   data: FormData
 ): Promise<{ client_secret: string | null; url: string | null }> {
   const ui_mode = data.get(
     "uiMode"
   ) as Stripe.Checkout.SessionCreateParams.UiMode;
-
+ 
   const origin: string = (await headers()).get("origin") as string;
-
   const locale = data.get("locale") || "en";
-
   const priceId = data.get("priceId") as string;
-
+  const name = data.get("name") as string;
+  const price = Number(data.get("price"));
+  const image = data.get("image") as string;
+ 
   if (!priceId) {
     throw new Error("Price ID is required.");
   }
-
+ 
+  const supabase = await createClient();
+  const userResponse = await supabase.auth.getUser();
+  const user_id = userResponse.data?.user?.id;
+ 
+  // Create Stripe checkout session
   const checkoutSession: Stripe.Checkout.Session =
     await stripe.checkout.sessions.create({
       mode: "payment",
@@ -39,13 +46,41 @@ export async function createCheckoutSession(
       }),
       ui_mode,
     });
-
+  const paymentIntent =
+    (await checkoutSession.payment_intent) as Stripe.PaymentIntent | null;
+  const paymentId = paymentIntent?.id;
+ 
+  try {
+    const { data: order, error: orderError } = await supabase
+      .from("orders")
+      .insert({
+        stripe_price_id: priceId,
+        user_id: user_id,
+        name: name,
+        price: price,
+        image: image,
+        payment_id: paymentId,
+      })
+      .select("id")
+      .single();
+ 
+    if (orderError) {
+      console.error("Error inserting order:", orderError);
+      throw new Error(`Failed to insert order: ${orderError.message}`);
+    }
+ 
+    console.log("Order inserted successfully:", order);
+  } catch (error) {
+    console.error("Error inserting order into database:", error);
+    throw new Error(`Failed to insert order: ${error}`);
+  }
+ 
   return {
     client_secret: checkoutSession.client_secret,
     url: checkoutSession.url,
   };
 }
-
+ 
 export async function createPaymentIntent(
   data: FormData
 ): Promise<{ client_secret: string }> {
